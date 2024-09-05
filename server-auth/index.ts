8bdf59707c2ca7bf81268c1ed5b565592c28bbc5
Mahmoud_Walid <modywmbadr@gmail.com>
2024-09-05 22:22:36 +0300

    create /api/auth route

--- server-auth/index.ts
+++ server-auth/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB_Mongo } from "./src/config/dbConfig";
 import userRouter from "./src/routes/user.route";
+import authRouter from "./src/routes/auth.route";
 
 dotenv.config({ path: "../.env" });
 
@@ -14,6 +15,7 @@ connectDB_Mongo();
 app.use(express.json());
 
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on http:/localhost:${port}`);
