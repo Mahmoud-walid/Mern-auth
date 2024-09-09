c27e515b9240fcd2e7d86686681d8237b391b2d2
Mahmoud_Walid <modywmbadr@gmail.com>
2024-09-09 15:55:12 +0300

    fix index start file

--- server-auth/index.ts
+++ server-auth/index.ts
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import { connectDB_Mongo } from "./src/config/dbConfig";
 import userRouter from "./src/routes/user.route";
 import authRouter from "./src/routes/auth.route";
+import { errorMiddleware } from "./src/middlewares/error.middleware";
 
 dotenv.config({ path: "../.env" });
 
@@ -16,6 +17,8 @@ app.use(express.json());
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+
+app.use(errorMiddleware)
 
 app.listen(port, () => {
   console.log(`Server is running on http:/localhost:${port}`);
